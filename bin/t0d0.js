972e97b9011fec491456827a5eec45a051ca62a4
Lesha Koss <regiusprod@gmail.com>
2015-02-06 21:04:33 +0600

    --days option to set review range, see #3

--- bin/t0d0.js
+++ bin/t0d0.js
@@ -136,7 +136,7 @@ var filterTodos = function(fullMap, options) {
           return;
         }
       } else if (todo.isReviewed) {
-        if (isBefore(subDays(today, 14), todo.reviewedAt)) {
+        if (isBefore(subDays(today, options.days || 14), todo.reviewedAt)) {
           // Ignore reviewed recently
           return;
         }
@@ -155,6 +155,10 @@ program
   .option(
     '-l, --lines <number>',
     'number of lines including TODO statement (default - 3)'
+  )
+  .option(
+    '-d, --days <number>',
+    'days since review is considered obsolete'
   )
   .option(
     '--all',
